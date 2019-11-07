4905bf0695cac3ab22bc54489b49fc9855e39964
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-11-07 18:03:28 -0500

    useLocalStorage file is filled in. Going to work on useDarkMode now

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -7,6 +7,9 @@ const useLocalStorage = (key, initValue) => {
     })
     const setValue = value => {
         setStoredValue(value)
-        
+        window.localStorage.setItem(key, JSON.stringify(value));
     }
+    return [storedValue, setValue];
 }
+
+export default useLocalStorage;
